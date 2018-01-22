9a87f1228131a229a2a63ca3b57d80e847c32af7
Novera Lim <lim.novera88@gmail.com>
2018-01-22 21:49:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHolidays(string) {
+  console.log('Happy Holidays!')
+}
+function happyHolidaysTo(string) {
+  console.log(`${Happyholidays}, ${name}!`)
+}
+function happyHolidayTo(string) {
+  console.log(`Happy ${holiday}, ${name}!`)
+}
