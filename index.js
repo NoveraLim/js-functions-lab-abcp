2d4efac62bc14624721d901de729eaad5f4c1e29
Novera Lim <lim.novera88@gmail.com>
2018-01-23 18:18:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function happyHolidays(string) {
-  return happyHolidays('Happy holidays!')
-  expect (happyHolidays()).toEqual('Happy holidays!')
+function happyHolidays() {
+  return "Happy holidays!"
 }
-function happyHolidaysTo(string) {
-  return happyHolidaysTo('`Happy holidays, ${name}!`')
-  expect (happyHolidaysTo('Maria')).toEqual('Happy holidays, Maria!')
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
-function happyHolidayTo(string) {
-  return happyHolidayTo('`Happy ${holiday}, ${name}!`')
-  expect(happyHolidayTo('Independence Day', 'Maria')).toEqual('Happy Independence Day, Maria!')
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(string) {
-  return holidayCountdown('`It\s ${days} days until ${holiday}!`')
-  expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
 }
+ Lock conversation
